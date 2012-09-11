75a3989eb48b3173385c02a1f863789f7da34a37
Matt <mhebel+github@gmail.com>
2012-09-11 15:53:34 -0300

    connect to postgresql

--- connect.php
+++ connect.php
@@ -7,11 +7,10 @@ $db_user    = 'test';
 $db_pass    = 'test';
 $db_name    = 'test';
 
-$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass
-      , array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+$db = new PDO("pgsql:host=$db_host;dbname=$db_name", $db_user, $db_pass)
 );
 
-mysql_select_db("test") or die(mysql_error());
+//mysql_select_db("test") or die(mysql_error());
 
 // display diamond next to admins/mods name, and output the username
 
